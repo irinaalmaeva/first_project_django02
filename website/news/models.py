3676e07044f448c982ec2d259ab443e4d8b936c3
irinaalmaeva <almaeva.irina@gmail.com>
2024-09-03 21:28:32 +0300

    add code models.py

--- website/news/models.py
+++ website/news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
+class News_post(models.Model):
+    title = models.CharField('Название новости', max_length=50)
+    short_description = models.CharField('Краткое описание новости', max_length=200)
+    text = models.TextField('Новость')
+    pub_date = models.DateTimeField('Дата публикации')
+    name_user = models.CharField('Имя пользователя', max_length=50)
+
+
+
+
 # Create your models here.
